be8b598f8c588155aebba28870f97bcc8aad5348
MelisaSosa <melisosa@alumno.unlam.edu.ar>
2021-06-13 21:17:12 -0300

    Muestra el porcentaje de batería al iniciar la app

--- app/src/main/java/org/unlam/covidapp/ui/login/RegistrarPatronActivity.java
+++ app/src/main/java/org/unlam/covidapp/ui/login/RegistrarPatronActivity.java
@@ -2,7 +2,11 @@ package org.unlam.covidapp.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,20 +24,37 @@ import io.paperdb.Paper;
 
 public class RegistrarPatronActivity extends AppCompatActivity {
 
-    String save_pattern_key = "pattern_code";
-    String final_pattern = "";
-    PatternLockView mPatternLockView;
+    private String save_pattern_key = "pattern_code";
+    private String final_pattern = "";
+    private PatternLockView mPatternLockView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = this.getApplicationContext().registerReceiver(null, ifilter);
+
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        float batteryPct = (level * 100) / (float)scale;
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Batería");
+        alertDialog.setMessage("La batería restante es de " + batteryPct + "%");
+        alertDialog.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alertDialog.show();
+
         Paper.init(this);
         final String save_pattern = Paper.book().read(save_pattern_key);
-        if(save_pattern != null && !save_pattern.equals("null"))
-        {
+        if(save_pattern != null && !save_pattern.equals("null")) {
             setContentView(R.layout.activity_principal);
-            mPatternLockView = (PatternLockView)findViewById(R.id.pattern_lock_view);
+            mPatternLockView = findViewById(R.id.pattern_lock_view);
             mPatternLockView.addPatternLockListener(new PatternLockViewListener() {
                 @Override
                 public void onStarted() {
@@ -64,12 +85,9 @@ public class RegistrarPatronActivity extends AppCompatActivity {
 
                 }
             });
-        }
-        else
-        {
-
+        } else {
             setContentView(R.layout.activity_main);
-            mPatternLockView = (PatternLockView)findViewById(R.id.pattern_lock_view);
+            mPatternLockView = findViewById(R.id.pattern_lock_view);
             mPatternLockView.addPatternLockListener(new PatternLockViewListener() {
                 @Override
                 public void onStarted() {
@@ -93,10 +111,7 @@ public class RegistrarPatronActivity extends AppCompatActivity {
                 }
             });
 
-
-
-
-            Button btnSetup = (Button)findViewById(R.id.btnGuardarPatron);
+            Button btnSetup = findViewById(R.id.btnGuardarPatron);
             btnSetup.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
